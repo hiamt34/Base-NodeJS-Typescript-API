e647f38abcbe94effa49d7782ee6d87c4d66a348
hiiamt34 <d17dccn195@gmail.com>
2022-03-30 20:12:09 +0700

    update sinleton

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,5 +1,9 @@
+import { ProductDocument } from "src/interfaces/products.interface";
+import { Product } from "../models";
 import { BaseService } from "./service";
 
 export class ProductService<T> extends BaseService<T> {
 
 }
+
+export const productService = new ProductService<ProductDocument>(Product)
